f1d41e2924047b448c612910fe48e35a76d5bc33
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2023-11-17 12:16:23 -0500

    update mid convert2bids files

--- code/convert2bids_mid.py
+++ code/convert2bids_mid.py
@@ -9,36 +9,38 @@ from natsort import natsorted
 import re
 
 #Make a list of the files
-Mat_Beh_dir='/ZPOOL/data/projects/istart-datapaper-test04/bids/sourcedata'
+Mat_Beh_dir='/ZPOOL/data/projects/istart-datapaper-test05/bids/sourcedata'
+#Mat_Beh_dir='/Users/jameswyngaarden/Documents/Github/istart-datapaper-test05/bids/sourcedata'
 ML_EvFiles_list=natsorted([os.path.join(root,f) for root,dirs,files in os.walk(
-    Mat_Beh_dir) for f in files if 'run' in f])
+    Mat_Beh_dir) for f in files if 'output.mat' in f])
 
 print("The number of Subjects are: %s"%(len([x for x in os.listdir(Mat_Beh_dir) if x.startswith('sub')])))
 print("The number of files are: %s"%(len(ML_EvFiles_list)))
 print("----------------------------------------")
-#Make Conditions for Run1 and Run2
-run_cond=loadmat('%s/../timing/run1.mat'%(Mat_Beh_dir))
-run1=run_cond['run']['cond'][0,0]
-run1 = np.where(run1==[1], 'Large_gain', run1)
-run1 = np.where(run1==['2'], 'Large_loss', run1)
-run1 = np.where(run1==['3'], 'Small_gain', run1)
-run1 = np.where(run1==['4'], 'Small_loss', run1)
-run1 = np.where(run1==['5'], 'Neutral', run1)
-run1=np.concatenate(run1)
-
-run_cond=loadmat('%s/../timing/run2.mat'%(Mat_Beh_dir))
-run2=run_cond['run']['cond'][0,0]
-run2 = np.where(run2==[1], 'Large_gain', run2)
-run2 = np.where(run2==['2'], 'Large_loss', run2)
-run2 = np.where(run2==['3'], 'Small_gain', run2)
-run2 = np.where(run2==['4'], 'Small_loss', run2)
-run2 = np.where(run2==['5'], 'Neutral', run2)
-run2=np.concatenate(run2)
 
 #Load the .mat convert to dataframe add trial_type and detect bad data
 for ML_EvFile in ML_EvFiles_list:
     print(ML_EvFile)
     sub='sub-'+re.search('sub-(.*)/',ML_EvFile).group(1)
+    # Make conditions for Run 1 and Run 2
+    run_cond=loadmat('%s/timing/run1.mat'%(Mat_Beh_dir))
+    run1=run_cond['run']['cond'][0,0]
+    run1 = np.where(run1==[1], 'Large_gain', run1)
+    run1 = np.where(run1==['2'], 'Large_loss', run1)
+    run1 = np.where(run1==['3'], 'Small_gain', run1)
+    run1 = np.where(run1==['4'], 'Small_loss', run1)
+    run1 = np.where(run1==['5'], 'Neutral', run1)
+    run1=np.concatenate(run1)
+
+    run_cond=loadmat('%s/timing/run2.mat'%(Mat_Beh_dir))
+    run2=run_cond['run']['cond'][0,0]
+    run2 = np.where(run2==[1], 'Large_gain', run2)
+    run2 = np.where(run2==['2'], 'Large_loss', run2)
+    run2 = np.where(run2==['3'], 'Small_gain', run2)
+    run2 = np.where(run2==['4'], 'Small_loss', run2)
+    run2 = np.where(run2==['5'], 'Neutral', run2)
+    run2=np.concatenate(run2)
+
     run=re.search('run-(.*)_out',ML_EvFile).group(1)
     print(sub,run)
     mat = loadmat(ML_EvFile)  # load mat-file
@@ -78,8 +80,8 @@ for ML_EvFile in ML_EvFiles_list:
         tmp2["trial_type"] = tmp2["outcome"].map(map_dict)
         tmp2=tmp2[['onset','duration','trial_type']]
         df=pd.concat([tmp1,tmp2])
-        if not os.path.isdir('../../istart-datapaper-test-04/bids/%s/func'%(sub)):
-            os.makedirs('../../istart-datapaper-test04/bids/%s/func'%(sub)) 
-        df.to_csv('../../istart-datapaper-test04/bids/%s/func/%s_task-mid_run-%s_events.tsv'%(sub,sub,run),sep = '\t', index=False)
+        if not os.path.isdir('../../istart-datapaper-test05/bids/%s/func'%(sub)):
+            os.makedirs('../../istart-datapaper-test05/bids/%s/func'%(sub)) 
+        df.to_csv('../../istart-datapaper-test05/bids/%s/func/%s_task-mid_run-%s_events.tsv'%(sub,sub,run),sep = '\t', index=False)
     else:
         print("Problem with %s run-%s"%(sub,run))
